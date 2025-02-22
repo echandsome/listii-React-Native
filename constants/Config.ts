a885298b668ad965f276eeabeecfc546a59365ca
echandsome <kingstar970106@gmail.com>
2025-02-22 18:59:20 -0500

    stress2

--- constants/Config.ts
+++ constants/Config.ts
@@ -3,6 +3,9 @@ import {
     Platform,
 } from 'react-native';
 
+const NEXT_PUBLIC_SUPABASE_URL = 'https://qtgkheaclxgwfrpjiqdv.supabase.co';
+const NEXT_PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0Z2toZWFjbHhnd2ZycGppcWR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzAzNDM5NzYsImV4cCI6MTk4NTkxOTk3Nn0.4Jq36bAv5ReZtqeNK-59r1x8xLX8I2R-F9E4X9zVzg4';
+
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 // const baseFontSize = Math.min(screenWidth, screenHeight) * 0.038;
@@ -15,6 +18,8 @@ const tbl_names = {
 }
 
 export  {
+    NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY,
     screenWidth,
     screenHeight,
     baseFontSize,
--- supabase.ts
+++ supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js"
-import { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } from "@env";
+import { NEXT_PUBLIC_SUPABASE_URL, NEXT_PUBLIC_SUPABASE_ANON_KEY } from "@/constants/Config";
 
 const supabaseUrl = NEXT_PUBLIC_SUPABASE_URL ?? '';
 const supabaseAnonKey = NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
